f7b23bd6400b3d25740d8cd7ddb6d74066f7912f
Zoogich2615 <albert1kamsu4@gmail.com>
2024-05-12 00:57:38 +0200

    ajouter par albert

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
-   
+
 
     /**
      * im Folgenden wollen wir ein Programm erstellen, das eine gleichung zweiter Grad mittels Methode von Diskriminant lösen kann
-     * dafür brauchen wir Parameter, mit denen  Variablen von folgendem Ausdruck (aX*X + bX + C) representiert werden.
+     * dafür brauchen wir Parameter, mit denen Variablen von folgendem Ausdruck (aX*X + bX + C) representiert werden.
      * @param a variable a
      * @param b variable b
      * @param c variable c
@@ -18,16 +18,27 @@ public class Main {
 
         X1 = ( -b - deltaWurzel /2*a);
         X2 = (-b + deltaWurzel )/2*a;
-        X0 = -b / 2*a
+        X0 = -b / 2*a;
 
-        if ( deltaWurzel > 0) { return( "Die Menge der Lösung ist  M = { " +X1+ " ; " +X2+ " }"); }
+        if ( deltaWurzel > 0) {
+            return ("Die Menge der Lösung ist  M = { " + X1 + " ; " + X2 + " }");
+        }
+        else if  (deltaWurzel == 0){ return( "Die Menge der Lösung ist  M = { " +X0+ " }"); }
+        else { return "es liegt keine Lösung für diese Gleichung vor"; }
+    }
 
-
-        else if (deltaWurzel = 0){ return( "Die Menge der Lösung ist  M = { " +X0+ " }"); }
-
-        else ( return "es liegt keine Lösung für diese Gleichung vor" );
-
-
- }
+public static void main(String[] args){ 
     
+    String result = Diskriminant(1,8,9);
+    
+    System.out.println(a); // Erwatete Ergebnis für diese Gleichung "es liegt keine Lösung für diese Gleichung vor"
+
+
+
 }
+
+}
+
+
+
+
